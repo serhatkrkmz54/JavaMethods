782d7316cf81a55ee33ca5d152658f4060a5a263
serhatkrkmz54 <korkmaz_serhat@hotmail.com>
2023-10-16 09:41:21 +0300

    javaMethods

--- src/Main.java
+++ src/Main.java
@@ -16,7 +16,29 @@ public class Main {
         System.out.println("Cevap: "+sonuc);
         return sonuc;
     }
-
+    static int bolme(int ilkSayi, int ikinciSayi){
+        int sonuc = ilkSayi/ikinciSayi;
+        System.out.println("Cevap: "+sonuc);
+        return sonuc;
+    }
+    static int usAlma(int usluSayi, int us){
+        int sonuc = 1;
+        for(int i=0;i<us;i++){
+            sonuc*=usluSayi;
+        }
+        System.out.println("Cevap: "+sonuc);
+        return sonuc;
+    }
+    static int modAlma(int modSayi, int mod){
+        int sonuc = modSayi%mod;
+        System.out.println("Cevap: "+sonuc);
+        return sonuc;
+    }
+    static int dikdortgenAlan(int kisaKenar, int uzunKenar){
+        int sonuc = kisaKenar*uzunKenar;
+        System.out.println("Cevap: "+sonuc);
+        return sonuc;
+    }
     public static void main(String[] args) {
         int select;
         Scanner giris = new Scanner(System.in);
@@ -35,6 +57,30 @@ public class Main {
             select = giris.nextInt();
             if(select==0)
                 break;
+            if(select==5){
+                System.out.print("Üslü Sayı Giriniz: ");
+                int usluSayi = giris.nextInt();
+                System.out.print("Üs Giriniz: ");
+                int us = giris.nextInt();
+                usAlma(usluSayi,us);
+                continue;
+            }
+            if(select==6){
+                System.out.print("Modu Alınacak Sayıyı Giriniz: ");
+                int modSayi = giris.nextInt();
+                System.out.print("Mod Giriniz: ");
+                int mod = giris.nextInt();
+                modAlma(modSayi,mod);
+                continue;
+            }
+            if(select==7){
+                System.out.print("Kısa Kenarı Giriniz: ");
+                int kisaKenar = giris.nextInt();
+                System.out.print("Uzun Kenarı Giriniz: ");
+                int uzunKenar = giris.nextInt();
+                dikdortgenAlan(kisaKenar,uzunKenar);
+                continue;
+            }
             System.out.println("İlk Sayıyı giriniz: ");
             int ilkSayi = giris.nextInt();
             System.out.println("İkinci Sayıyı Giriniz: ");
@@ -49,6 +95,9 @@ public class Main {
                     break;
                 case 3:
                     carpma(ilkSayi,ikinciSayi);
+                    break;
+                case 4:
+                    bolme(ilkSayi,ikinciSayi);
                     break;
             }
         }
